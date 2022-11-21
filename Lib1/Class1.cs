e0c7190cbd48836785ac7baad620729f783473ea
исп-31 <-31@LAB17-01>
2022-11-21 14:21:30 +0300

    Добавьте файлы проекта.

---
+++ Lib1/Class1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_1
+{
+    public class Car
+    {
+        string _marka;
+
+        int _numOfCylinders;
+
+        double _horsePower, _kW;
+
+        public string Marka
+        {
+            get => _marka;
+            set { if (value == "") _marka = null; else { _marka = value; } }
+        }
+
+        public double HorsePower
+        {
+            get => _horsePower;
+            set { if (value >= 0) _horsePower = value; _kW = value / 1.36; }
+        }
+        public double KW
+        {
+            get => _kW;
+            set { if (value >= 0) _kW = value; _horsePower = value * 1.36; }
+        }
+
+        public int NumOfCylinders
+        {
+            get => _numOfCylinders;
+            set { _numOfCylinders = value; }
+        }
+
+        public Car()
+        {
+            _marka = string.Empty;
+            _numOfCylinders = 0;
+            _kW = 0;
+        }
+
+        public Car(string marka, int numOfCylinders, double kw)
+        {
+            _marka = marka;
+            _numOfCylinders = numOfCylinders;
+            _kW = kw;
+        }
+/*
+        public static Car operator ++(Car car1)
+        {
+            return new Car { KW = car1.KW + 10 };
+        }
+*/
+
+        public static Car operator ++(Car car1)
+        {
+            car1.KW += 1;
+            return car1;
+        }
+
+
+
+        /// <summary>
+        /// Перегруженный метод ToString() для изменения строки представляющей объект в ListBox
+        /// </summary>
+        /// <returns>Возвращает марку автомобиля</returns>
+        public override string ToString()
+        {
+            return this._marka;
+        }
+
+        /// <summary>
+        /// Метод меняет параметры автомобиля
+        /// </summary>
+        /// <param name="marka">Марка автомобиля</param>
+        /// <param name="numOfCylinders">Кол-во целиндров</param>
+        /// <param name="kW">кВт</param>
+        public void SetParams(string marka, int numOfCylinders, double kW)
+        {
+            Marka = marka;
+            NumOfCylinders = numOfCylinders;
+            KW = kW;
+        }
+
+        /// <summary>
+        /// Метод меняет параметры автомобиля
+        /// </summary>
+        /// <param name="marka">Марка автомобиля</param>
+        /// <param name="numOfCylinders">Кол-во целиндров</param>
+        public void SetParams(string marka, int numOfCylinders)
+        {
+            Marka = marka;
+            NumOfCylinders = numOfCylinders;
+        }
+
+        /// <summary>
+        /// Метод меняет параметры автомобиля
+        /// </summary>
+        /// <param name="marka">Марка автомобиля</param>
+        /// <param name="kW">кВт</param>
+        public void SetParams(string marka, double kW)
+        {
+            Marka = marka;
+            KW = kW;
+        }
+
+        /// <summary>
+        /// Перегруженный оператор <
+        /// </summary>
+        /// <param name="car1">Автомобиль1</param>
+        /// <param name="car2">Автомобиль2</param>
+        /// <returns>Возвращает результат сравнения</returns>
+        public static bool operator <(Car car1, Car car2)
+        {
+            return (car1.NumOfCylinders < car2.NumOfCylinders) || (car1.KW < car2.KW);
+        }
+
+        /// <summary>
+        /// Перегруженный оператор >
+        /// </summary>
+        /// <param name="car1">Автомобиль1</param>
+        /// <param name="car2">Автомобиль2</param>
+        /// <returns>Возвращает результат сравнения</returns>
+        public static bool operator >(Car car1, Car car2)
+        {
+            return (car1.NumOfCylinders > car2.KW) || (car1.NumOfCylinders > car2.KW);
+        }
+
+
+        /// <summary>
+        /// Перегруженный оператор ==
+        /// </summary>
+        /// <param name="car1">Автомобиль1</param>
+        /// <param name="car2">Автомобиль2</param>
+        /// <returns>Возвращает результат сравнения</returns>
+        public static bool operator ==(Car car1, Car car2)
+        {
+            return (car1.NumOfCylinders == car2.NumOfCylinders) && (car1.KW == car2.KW);
+        }
+
+        /// <summary>
+        /// Перегруженный оператор !=
+        /// </summary>
+        /// <param name="car1">Автомобиль1</param>
+        /// <param name="car2">Автомобиль2</param>
+        /// <returns>Возвращает результат сравнения</returns>
+        public static bool operator !=(Car car1, Car car2)
+        {
+            return (car1.NumOfCylinders != car2.NumOfCylinders) && (car1.NumOfCylinders != car2.NumOfCylinders);
+        }
+
+        /*
+                /// <summary>
+                /// Перегруженный оператор <
+                /// </summary>
+                /// <param name="car1">Автомобиль</param>
+                /// <param name="value">Кол-во цилиндров</param>
+                /// <returns>Возвращает результат сравнения</returns>
+                public static bool operator <(Car car1, int value)
+                {
+                    return car1.NumOfCylinders < value;
+                }
+                /// <summary>
+                /// Перегруженный оператор >
+                /// </summary>
+                /// <param name="car1">Автомобиль</param>
+                /// <param name="value">Кол-во целиндров</param>
+                /// <returns>Возвращает результат сравнения</returns>
+                public static bool operator >(Car car1, int value)
+                {
+                    return car1.NumOfCylinders > value;
+                }
+
+                /// <summary>
+                /// Перегруженный оператор ==
+                /// </summary>
+                /// <param name="car1">Автомобиль</param>
+                /// <param name="value">Кол-во цилиндров</param>
+                /// <returns>Возвращает результат сравнения</returns>
+                public static bool operator ==(Car car1, int value)
+                {
+                    return car1.NumOfCylinders == value;
+                }
+
+                /// <summary>
+                /// Перегруженный оператор !=
+                /// </summary>
+                /// <param name="car1">Автомобиль</param>
+                /// <param name="value">Кол-во цилиндров</param>
+                /// <returns>Возвращает результат сравнения</returns>
+                public static bool operator !=(Car car1, int value)
+                {
+                    return car1.NumOfCylinders != value;
+                }
+
+                /// <summary>
+                /// Перегруженный оператор <
+                /// </summary>
+                /// <param name="car1">Автомобиль</param>
+                /// <param name="value">кВт</param>
+                /// <returns>Возвращает результат сравнения</returns>
+                public static bool operator <(Car car1, double value)
+                {
+                    return car1.KW < value;
+                }
+
+                /// <summary>
+                /// Перегруженный оператор >
+                /// </summary>
+                /// <param name="car1">Автомобиль</param>
+                /// <param name="value">кВт</param>
+                /// <returns>Возвращает результат сравнения</returns>
+                public static bool operator >(Car car1, double value)
+                {
+                    return car1.KW > value;
+                }
+
+                /// <summary>
+                /// Перегруженный оператор ==
+                /// </summary>
+                /// <param name="car1">Автомобиль</param>
+                /// <param name="value">кВт</param>
+                /// <returns>Возвращает результат сравнения</returns>
+                public static bool operator ==(Car car1, double value)
+                {
+                    return car1.KW == value;
+                }
+
+                /// <summary>
+                /// Перегруженный оператор !=
+                /// </summary>
+                /// <param name="car1">Автомобиль</param>
+                /// <param name="value">кВт</param>
+                /// <returns>Возвращает результат сравнения</returns>
+                public static bool operator !=(Car car1, double value)
+                {
+                    return car1.KW != value;
+                }*/
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,275 @@
+﻿using Lib_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace WPF_PR5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //int kol = 0;
+
+        //List<Object> cars = new List<Object>();
+
+        Car car;
+        object[] list;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            comboBox1.MaxDropDownHeight = 100;
+            comboBox1.ItemsSource = listBoxCars.Items;
+            comboBox2.MaxDropDownHeight = 100;
+            //comboBox2.ItemsSource = listBoxCars.Items;
+            comboBox2.ItemsSource = list;
+        }
+
+        void Print(Car car)
+        {
+            textBoxOutLog.Text = $"Марка: {car.Marka ?? "Без названия"}" +
+                    $"\nКол-во целиндров: {car.NumOfCylinders}"
+                    + $"\nМощность двигателя в Л.С.: {car.HorsePower}"
+                    + $"\nМощность двигателя в  кВт: {car.KW}";
+        }
+
+        private void ZapisButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBoxCars.SelectedItem != null)
+            {
+                car = listBoxCars.SelectedItem as Car;
+
+                double kW;
+                int numCylinders;
+
+                if (int.TryParse(textBoxNumCylinders.Text, out numCylinders) && double.TryParse(textBoxkW.Text, out kW))
+                {
+                    car.SetParams(textBoxMarka.Text, numCylinders, kW);
+                }
+                else if (int.TryParse(textBoxNumCylinders.Text, out numCylinders))
+                {
+                    car.KW = new double();
+                    car.SetParams(textBoxMarka.Text, numCylinders);
+                }
+                else if (double.TryParse(textBoxkW.Text, out kW))
+                {
+                    car.NumOfCylinders = new int();
+                    car.SetParams(textBoxMarka.Text, kW);
+                }
+                else
+                {
+                    car.SetParams(textBoxMarka.Text, new int(), new double());
+                }
+                listBoxCars.Items.Insert(listBoxCars.SelectedIndex, car);
+                listBoxCars.Items.RemoveAt(listBoxCars.SelectedIndex);
+                comboBox1.ItemsSource = listBoxCars.Items;
+
+                list = listBoxCars.Items.OfType<object>().ToArray();
+                comboBox2.ItemsSource = list;
+            }
+            else
+            {
+                MessageBox.Show("Выберите машину","Error!");
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxMarka.Text != string.Empty)
+            {
+                double kW;
+                int numCylinders;
+                listBoxCars.Items.Add(new Car());
+
+                car = listBoxCars.Items[listBoxCars.Items.Count - 1] as Car;
+
+
+                if (int.TryParse(textBoxNumCylinders.Text, out numCylinders) && double.TryParse(textBoxkW.Text, out kW))
+                {
+                    car.SetParams(textBoxMarka.Text, numCylinders, kW);
+                }
+                else if (int.TryParse(textBoxNumCylinders.Text, out numCylinders))
+                {
+                    car.KW = new double();
+                    car.SetParams(textBoxMarka.Text, numCylinders);
+                }
+                else if (double.TryParse(textBoxkW.Text, out kW))
+                {
+                    car.NumOfCylinders = new int();
+                    car.SetParams(textBoxMarka.Text, kW);
+                }
+                else
+                {
+                    car.SetParams(textBoxMarka.Text, new int(), new double());
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Введите название для машины", "Error!");
+            }
+
+            list = listBoxCars.Items.OfType<object>().ToArray();
+            comboBox2.ItemsSource = list;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void VivodButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBoxCars.SelectedItem != null)
+            {
+                car = listBoxCars.SelectedItem as Car;
+
+                Print(car);
+            }
+            else
+            {
+                MessageBox.Show("Выберите машину", "Error!");
+            }
+        }
+
+        private void SravnitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                Car car1 = comboBox1.SelectedItem as Car;
+                Car car2 = comboBox2.SelectedItem as Car;
+
+                if (car1 > car2)
+                {
+                    textBoxOutLog2.Text = $"Автомобиль {car1.Marka} круче {car2.Marka}";
+                }
+                else if(car1 < car2)
+                {
+                    textBoxOutLog2.Text = $"Автомобиль {car2.Marka} круче {car1.Marka}";
+                }
+                else if (car1 == car2)
+                {
+                    textBoxOutLog2.Text = $"Автомобиль {car2.Marka} равен {car1.Marka} по характеристикам";
+                }
+
+/*
+                string info;
+
+                if (car1 > car2.NumOfCylinders)
+                {
+                    operator1.Content = ">";
+                    info = $"Автомобиль {car1.Marka} лучше {car2.Marka} в кол-ве цилиндров на {car1.NumOfCylinders - car2.NumOfCylinders}.";
+                }
+                else if (car1 == car2.NumOfCylinders)
+                {
+                    operator1.Content = "=";
+                    info = $"Автомобиль {car1.Marka} равен {car2.Marka} в кол-ве цилиндров.";
+                }
+                else
+                {
+                    operator1.Content = "<";
+                    info = $"Автомобиль {car1.Marka} устуает {car2.Marka} в кол-ве цилиндров {car2.NumOfCylinders - car1.NumOfCylinders}.";
+                }
+
+
+                if (car1 > car2.KW)
+                {
+                    operator2.Content = ">";
+                    info += $"\nАвтомобиль {car1.Marka} лучше {car2.Marka} в мощности на {car1.KW - car2.KW}.";
+                }
+                else if (car1 == car2.KW)
+                {
+                    operator2.Content = "=";
+                    info += $"\nАвтомобиль {car1.Marka} равен {car2.Marka} в мощности.";
+                }
+                else
+                {
+                    operator2.Content = "<";
+                    info += $"\nАвтомобиль {car1.Marka} уступает {car2.Marka} в мощности на {car2.KW - car1.KW}.";
+                }
+                textBoxOutLog2.Text = info;*/
+            }
+            else
+            {
+                MessageBox.Show("Отсутствуют автомобили", "Error!");
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxCars.Items.Remove(listBoxCars.SelectedItem);
+
+            list = listBoxCars.Items.OfType<object>().ToArray();
+            comboBox2.ItemsSource = list;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                car = comboBox1.SelectedItem as Car;
+                textBoxPower1.Text = car.HorsePower.ToString();
+                textBoxCylinders1.Text = car.NumOfCylinders.ToString();
+            }
+            else
+            {
+                textBoxPower1.Text = String.Empty;
+                textBoxCylinders1.Text = String.Empty;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox2.SelectedIndex != -1)
+            {
+                car = comboBox2.SelectedItem as Car;
+                textBoxPower2.Text = car.HorsePower.ToString();
+                textBoxCylinders2.Text = car.NumOfCylinders.ToString();
+            }
+            else
+            {
+                textBoxPower2.Text = String.Empty;
+                textBoxCylinders2.Text = String.Empty;
+            }
+        }
+
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Задание:\nИспользовать базовый класс Саr (машина), характеризуемый торговой маркой \r\n(строка), числом цилиндров, мощностью. Разработать операции для определения \r\nкрутости машин. Машина считается круче, если у одной машины количество \r\nцилиндров и мощность больше чем у другой машины или при равенстве одного из \r\nпараметров второй параметр больше. Разработать операцию увеличение мощности \r\nна 1.\r\n", "О программе");
+        }
+
+        private void PlusButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBoxCars.SelectedItem !=null)
+            {
+                car = listBoxCars.SelectedItem as Car;
+                car++;
+                listBoxCars.SelectedItem = car;
+
+                Print(car);
+
+                list = listBoxCars.Items.OfType<object>().ToArray();
+                //ItemsControl.ItemsSource
+                //comboBox2.DataContext = null;
+                //comboBox2.Items.Clear();
+                //comboBox1.ItemsSource = list;
+                comboBox2.ItemsSource = list;
+                comboBox1.SelectedIndex = -1;
+                comboBox2.SelectedIndex = -1;
+            }
+            else
+            {
+                MessageBox.Show("Выберите машину, которой будете увеличивать мощность", "Error!");
+            }
+        }
+    }
+}
